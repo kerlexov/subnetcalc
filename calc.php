e16e1948abd7222c7b935733f70b6dd4357a95c0
kerlexov <kerlexov@gmail.com>
2019-01-21 13:02:20 +0100

    update

--- calc.php
+++ calc.php
@@ -5,26 +5,20 @@ include 'submreza.php';
 $poljeaddr=array();
 $poljemaska=array();
 //adresa i subnet maska po kojoj se sumeriziraju unesene mreze
-$adresa='192.168.10.0';
-$maska=24;
+//$adresa='192.168.10.0';
+$adresa=$_POST["ip"];
+//$maska=24;
+//$maska=$_POST["maska"];
+$klasa=$_POST["klasa"];
 $raz="</br></br>";$sumhost=0;$sumhostm=0;
 //TODO treba unos prilagodit korisniku
-$subnet0=62;$subnet1=126;$subnet2=30;$subnet3=0;$subnet4=0;$subnet5=0;$subnet6=0;$subnet7=0;$subnet8=0;$subnet9=0;
-if($subnet0>0){    array_push($poljeaddr,$subnet0);}
-if($subnet1>0){    array_push($poljeaddr,$subnet1);}
-if($subnet2>0){    array_push($poljeaddr,$subnet2);}
-if($subnet3>0){    array_push($poljeaddr,$subnet3);}
-if($subnet4>0){    array_push($poljeaddr,$subnet4);}
-if($subnet5>0){    array_push($poljeaddr,$subnet5);}
-if($subnet6>0){    array_push($poljeaddr,$subnet6);}
-if($subnet7>0){    array_push($poljeaddr,$subnet7);}
-if($subnet8>0){    array_push($poljeaddr,$subnet8);}
-if($subnet9>0){    array_push($poljeaddr,$subnet9);}
+$subnet0=$_POST["subnet0"];$subnet1=$_POST["subnet1"];$subnet2=$_POST["subnet2"];$subnet3=$_POST["subnet3"];$subnet4=$_POST["subnet4"];$subnet5=$_POST["subnet5"];$subnet6=$_POST["subnet6"];$subnet7=$_POST["subnet7"];$subnet8=$_POST["subnet8"];$subnet9=$_POST["subnet9"];
+if($subnet0>0){    array_push($poljeaddr,$subnet0);}if($subnet1>0){    array_push($poljeaddr,$subnet1);}if($subnet2>0){    array_push($poljeaddr,$subnet2);}if($subnet3>0){    array_push($poljeaddr,$subnet3);}if($subnet4>0){    array_push($poljeaddr,$subnet4);}if($subnet5>0){    array_push($poljeaddr,$subnet5);}if($subnet6>0){    array_push($poljeaddr,$subnet6);}if($subnet7>0){    array_push($poljeaddr,$subnet7);}if($subnet8>0){    array_push($poljeaddr,$subnet8);}if($subnet9>0){    array_push($poljeaddr,$subnet9);}
 //sortiraj polje korisnickih zahtjeva (npr broj racunala u ucionici)
 rsort($poljeaddr);
 $arrlen = count($poljeaddr);
 for($x = 0; $x < $arrlen; $x++) {
-    echo $poljeaddr[$x];
+//   echo $poljeaddr[$x];
     $sumhost+=$poljeaddr[$x];
     for ($i=0;$i<=32;$i++){
         $s=(2**$i-2)-$poljeaddr[$x];
@@ -35,12 +29,13 @@ for($x = 0; $x < $arrlen; $x++) {
                 break;}
         }
     }
-    echo "/";
-    echo $poljemaska[$x];
-    echo "<br>";
+//    echo "/";
+//    echo $poljemaska[$x];
+//    echo "<br>";
 }
-if($maska>=$poljemaska[0]){    echo "mala maska";}
-echo "</br>";
+//maska usporedba
+//if($maska>=$poljemaska[0]){    echo "mala maska";}
+//echo "</br>";
 for ($i=0;$i<=32;$i++){
     $s=(2**$i-2)-$sumhost;
     if($s>=0){
@@ -48,64 +43,93 @@ for ($i=0;$i<=32;$i++){
         break;
     }
 }
-echo $sumhost;
+//echo $sumhost;
+echo $adresa;
 echo "/";
 echo $sumhostm;
-echo "</br>";
-
-//nulta mreza(najveca)
-$sub0=new mreza(0,$adresa,$poljemaska[0]);
-echo $sub0->getReport();
 echo $raz;
+?>
+<html>
+<head>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <table>
+            <thead>
+            <tr>
+                <th>summary</th>
+            </tr>
+            </thead>
+            <tbody>
+<?php
+//nulta mreza(najveca)
+$sub0=new mreza(0,$adresa,$poljemaska[0]);?>
+<tr>
+    <td><?php echo $sub0->getReport();?></td>
+</tr>
+<?php
 //prva mreza
 if($poljemaska[1]>0){
-    $sub1=new mreza(1,$sub0->getSledeca(),$poljemaska[1]);
-    echo $sub1->getReport();
-    echo $raz;}
+    $sub1=new mreza(1,$sub0->getSledeca(),$poljemaska[1]);?>
+    <tr>
+    <td><?php echo $sub0->getReport();}?></td>
+</tr>
+ <?php
 //druga mreza
 if($poljemaska[2]>0){
-    $sub2=new mreza(2,$sub1->getSledeca(),$poljemaska[2]);
-    echo $sub2->getReport();
-    echo $raz;}
+    $sub2=new mreza(2,$sub1->getSledeca(),$poljemaska[2]);?>
+    <tr>
+    <td><?php echo $sub2->getReport();}?></td>
+</tr>
+<?php
 //treca
 if($poljemaska[3]>0){
-    $sub3=new mreza(3,$sub2->getSledeca(),$poljemaska[3]);
-    echo $sub3->getReport();
-    echo $raz;}
+    $sub3=new mreza(3,$sub2->getSledeca(),$poljemaska[3]);?>
+   <tr>
+    <td><?php echo $sub0->getReport();}?></td>
+</tr>
+<?php
 //cet
 if($poljemaska[4]>0){
-    $sub4=new mreza(4,$sub3->getSledeca(),$poljemaska[4]);
-    echo $sub4->getReport();
-    echo $raz;}
+    $sub4=new mreza(4,$sub3->getSledeca(),$poljemaska[4]);?>
+     <tr>
+    <td><?php echo $sub0->getReport();}?></td>
+</tr>
+<?php
 //peta
 if($poljemaska[5]>0){
-    $sub5=new mreza(5,$sub4->getSledeca(),$poljemaska[5]);
-    echo $sub5->getReport();
-    echo $raz;}
+    $sub5=new mreza(5,$sub4->getSledeca(),$poljemaska[5]);?>
+     <tr>
+    <td><?php echo $sub0->getReport();}?></td>
+</tr>
+<?php
 //sesta
 if($poljemaska[6]>0){
-    $sub6=new mreza(6,$sub5->getSledeca(),$poljemaska[6]);
-    echo $sub6->getReport();
-    echo $raz;}
+    $sub6=new mreza(6,$sub5->getSledeca(),$poljemaska[6]);?>
+  <tr>
+    <td><?php echo $sub0->getReport();}?></td>
+</tr>
+<?php
 //sedma
 if($poljemaska[7]>0){
-    $sub7=new mreza(7,$sub6->getSledeca(),$poljemaska[7]);
-    echo $sub7->getReport();
-    echo $raz;}
+    $sub7=new mreza(7,$sub6->getSledeca(),$poljemaska[7]);?>
+    <tr>
+    <td><?php echo $sub0->getReport();}?></td>
+</tr>
+<?php
 //osma
 if($poljemaska[8]>0){
-    $sub8=new mreza(8,$sub7->getSledeca(),$poljemaska[8]);
-    echo $sub8->getReport();
-    echo $raz;}
+    $sub8=new mreza(8,$sub7->getSledeca(),$poljemaska[8]);?>
+    <tr>
+    <td><?php echo $sub0->getReport();}?></td>
+</tr>
+<?php
 //deveta
 if($poljemaska[9]>0){
-    $sub9=new mreza(9,$sub8->getSledeca(),$poljemaska[9]);
-    echo $sub9->getReport();
-    echo $raz;}
-/*
-//gets ip and mask from user
-$sub=new \IPv4\SubnetCalculator($ip,$mask);
-include 'calc.php';
-
-//$sub->getSubnetJSONReport();
-*/
+    $sub9=new mreza(9,$sub8->getSledeca(),$poljemaska[9]);?>
+    <tr>
+    <td><?php echo $sub0->getReport();}?></td>
+</tr>
+?>
--- index.php
+++ index.php
@@ -18,49 +18,49 @@
             <div class="col s5">
                 <label>Ip adresa:</label> <input type="text" name="ip" value="192.168.0.0" required><br>
             </div>
-            <div class="col s3 m2">
+            <!--<div class="col s3 m2">
                 <label>Subnet maska:</label> <input type="text" name="maska" value="24" required><br>
-            </div>
+            </div>-->
             <div class="input-field col s6 m2">
                 <select>
                     <option value="" disabled selected>Odaberi klasu</option>
-                    <option value="1">A</option>
-                    <option value="2">B</option>
-                    <option value="3">C</option>
+                    <option name="klasa" value="a">A</option>
+                    <option name="klasa" value="b">B</option>
+                    <option name="klasa" value="c">C</option>
                 </select>
                 <label>Klase</label>
             </div>
     </div>
     <div class="row">
     <div class="input-field col s1">Broj uredaja 1. mreze
-        <input type="number" name="subnet0" id="subnet0" minlength="1" maxlength="10" value="0" placeholder="10" required><br>
+        <input type="number" name="subnet0" id="subnet0" minlength="1" maxlength="10" value="10" placeholder="10" required><br>
     </div>
     <div class="input-field col s1">Broj uredaja 2. mreze
-        <input type="number" name="subnet0" id="subnet1" minlength="1" maxlength="10" value="0" placeholder="10" required><br>
+        <input type="number" name="subnet1" id="subnet1" minlength="1" maxlength="10" value="0" placeholder="10" required><br>
     </div>
     <div class="input-field col s1">Broj uredaja 3. mreze
-        <input type="number" name="subnet0" id="subnet2" minlength="1" maxlength="10" value="0" placeholder="10" required><br>
+        <input type="number" name="subnet2" id="subnet2" minlength="1" maxlength="10" value="0" placeholder="10" required><br>
     </div>
     <div class="input-field col s1">Broj uredaja 4. mreze
-        <input type="number" name="subnet0" id="subnet3" minlength="1" maxlength="10" value="0" placeholder="10" required><br>
+        <input type="number" name="subnet3" id="subnet3" minlength="1" maxlength="10" value="0" placeholder="10" required><br>
     </div>
     <div class="input-field col s1">Broj uredaja 5. mreze
-        <input type="number" name="subnet0" id="subnet4" minlength="1" maxlength="10" value="0" placeholder="10" required><br>
+        <input type="number" name="subnet4" id="subnet4" minlength="1" maxlength="10" value="0" placeholder="10" required><br>
     </div>
     <div class="input-field col s1">Broj uredaja 6. mreze
-        <input type="number" name="subnet0" id="subnet5" minlength="1" maxlength="10" value="0" placeholder="10" required><br>
+        <input type="number" name="subnet5" id="subnet5" minlength="1" maxlength="10" value="0" placeholder="10" required><br>
     </div>
     <div class="input-field col s1">Broj uredaja 7. mreze
-        <input type="number" name="subnet0" id="subnet6" minlength="1" maxlength="10" value="0" placeholder="10" required><br>
+        <input type="number" name="subnet6" id="subnet6" minlength="1" maxlength="10" value="0" placeholder="10" required><br>
     </div>
     <div class="input-field col s1">Broj uredaja 8. mreze
-        <input type="number" name="subnet0" id="subnet7" minlength="1" maxlength="10" value="0" placeholder="10" required><br>
+        <input type="number" name="subnet7" id="subnet7" minlength="1" maxlength="10" value="0" placeholder="10" required><br>
     </div>
     <div class="input-field col s1">Broj uredaja 9. mreze
-        <input type="number" name="subnet0" id="subnet8" minlength="1" maxlength="10" value="0" placeholder="10" required><br>
+        <input type="number" name="subnet8" id="subnet8" minlength="1" maxlength="10" value="0" placeholder="10" required><br>
     </div>
     <div class="input-field col s1">Broj uredaja 10. mreze
-        <input type="number" name="subnet0" id="subnet9" minlength="1" maxlength="10" value="0" placeholder="10" required><br>
+        <input type="number" name="subnet9" id="subnet9" minlength="1" maxlength="10" value="0" placeholder="10" required><br>
     </div>
 
     </div>
